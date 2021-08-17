9c1a876482a6493ad65894fbb99c5a69ad0a2000
David Brendel <mantus667@googlemail.com>
2021-08-17 20:03:28 +0200

    feat: Added UnitTests
    Added UnitTests project and new build task to run tests

--- src/build/Build.cs
+++ src/build/Build.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using System.Linq;
 using Nuke.Common;
 using Nuke.Common.CI;
@@ -9,6 +10,7 @@ using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
 using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Tools.GitVersion;
+using Nuke.Common.Utilities;
 using Nuke.Common.Utilities.Collections;
 using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
@@ -25,7 +27,7 @@ class Build : NukeBuild
     ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
     ///   - Microsoft VSCode           https://nuke.build/vscode
 
-    public static int Main () => Execute<Build>(x => x.Compile);
+    public static int Main () => Execute<Build>(x => x.Test);
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
@@ -36,6 +38,7 @@ class Build : NukeBuild
 
     AbsolutePath SourceDirectory => RootDirectory / "src";
     AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
+    AbsolutePath TestResultDirectory => RootDirectory / "test_results";
 
     Target Clean => _ => _
         .Before(Restore)
@@ -63,6 +66,31 @@ class Build : NukeBuild
                 .SetFileVersion(GitVersion.AssemblySemFileVer)
                 .SetInformationalVersion(GitVersion.InformationalVersion)
                 .EnableNoRestore());
+        });
+
+    Target Test => _ => _
+        .DependsOn(Compile)
+        .Executes(() =>
+        {
+            var project = Solution.GetProject("DummyFramework.UnitTests");
+            var projectDirectory = Path.GetDirectoryName(project.Path);
+            string testFile = TestResultDirectory / $"unittests.testresults";
+            // This is so that the global dotnet is used instead of the one that comes with NUKE
+            var dotnetPath = ToolPathResolver.GetPathExecutable("dotnet");
+
+            DotNetTest(s => s
+            .SetProjectFile(project.Path.ToString())
+            .SetConfiguration(Configuration)
+            .EnableNoBuild()
+            .EnableNoRestore()
+            .SetProcessWorkingDirectory(projectDirectory));
+            //ProcessTasks.StartProcess(dotnetPath, "xunit " +
+            //                         "-nobuild " +
+            //                         $"-xml {testFile.DoubleQuoteIfNeeded()}",
+            //        workingDirectory: projectDirectory)
+            //    // AssertWairForExit() instead of AssertZeroExitCode()
+            //    // because we want to continue all tests even if some fail
+            //    .AssertWaitForExit();
         });
 
     Target Pack => _ => _
